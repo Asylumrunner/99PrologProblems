05f659d07242996b95133c547192ee3c4b2525de
Asylumrunner <asylumrunner@gmail.com>
2015-12-16 23:16:09 -0600

    Made template for arithmetic problems

---
+++ 2 - Arithmetic/arithmetic.pl
@@ -0,0 +1,23 @@
+% A knowledge base written for the arithmetic problems of Werner Hett's 99 Prolog Problems
+
+% 2.01 Determine whether a given integer is prime
+
+% 2.02 Detemine the prime factors of a positive integer
+
+% 2.03 Determine the prime factors of a given positive integer(2)
+
+% 2.04 A list of prime numbers
+
+% 2.05 Goldbach's conjecture
+
+% 2.06 A list of Goldbach's compositions
+
+% 2.07 Determine the greatest common divisor of two positive integer numbers
+
+% 2.08 Deterine whether two positive integer numbers are coprime
+
+% 2.09 Calculate Euler's totient function phi(m)
+
+% 2.10 Calculate Euler's totient function phi(m)(2)
+
+% 2.11 Compare the two methods of calculating Euler's totient function
